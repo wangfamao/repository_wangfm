5786093eee3c918dcd253d639935accf85231ff6
Administrator <Administrator@80DR054LIU0TTAN>
2018-05-31 11:54:02 +0800

    test

--- src/com/entor/test/Test2.java
+++ src/com/entor/test/Test2.java
@@ -1,9 +1,9 @@
 package com.entor.test;
 
-public class Test {
+public class Test2 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World1");;
+		System.out.println("Hello World");
 	}
 }
